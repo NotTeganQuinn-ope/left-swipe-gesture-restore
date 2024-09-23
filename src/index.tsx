1d02f556fef5048c2cce9d61d8ee2b9c29ebce2a
NotTeganQuinn-ope <notteganquinn@aol.com>
2024-09-22 23:56:32 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,184 +1,37 @@
-import { findByProps, findByStoreName } from "@vendetta/metro";
-import { ReactNative } from "@vendetta/metro/common";
-import { after, before, instead } from "@vendetta/patcher";
-import { storage, manifest } from "@vendetta/plugin";
-import Settings from "./components/Settings";
-import { DefaultNativeEvent, DoubleTapStateProps, Plugin, NativeEvent } from "./def";
-import { findInReactTree } from "@vendetta/utils";
+// Vendetta Plugin to Restore Swipe Left for Member List
+import { registerPlugin } from '@vendetta/plugin';
+import { findByProps } from "@vendetta/metro";
+import { getByName } from "@vendetta/metro/filters";
+import { before } from "@vendetta/patcher";
 
-const ChatInputRef = findByProps("insertText");
-const ChannelStore = findByStoreName("ChannelStore");
-const MessageStore = findByStoreName("MessageStore");
-const UserStore = findByStoreName("UserStore");
-const Messages = findByProps("sendMessage", "startEditMessage");
-const ReplyManager = findByProps("createPendingReply");
-const { MessagesHandlers } = findByProps("MessagesHandlers");
+const Drawer = findByProps("openDrawer", "closeDrawer");
+const MembersList = getByName("GuildChannelMembers");
 
-const BetterChatGestures: Plugin = {
-    unpatchGetter: null,
-    unpatchHandlers: null,
-    currentTapIndex: 0,
+let patcher;
 
-    doubleTapState({ state = "UNKNOWN", nativeEvent }: DoubleTapStateProps) {
-        const stateObject = {
-            state,
-            data: nativeEvent
-        };
-
-        if (state == "INCOMPLETE") {
-            Object.assign(stateObject, {
-                reason: {
-                    required: {
-                        taps: 2,
-                        isAuthor: true
-                    },
-                    received: {
-                        taps: stateObject.data.taps,
-                        isAuthor: stateObject.data.isAuthor
-                    }
-                }
-            })
-        }
-
-        return manifest.authors.find(author => author.id === UserStore.getCurrentUser().id)
-            ? console.log("DoubleTapState", stateObject)
-            : void 0;
-    },
-
-    patchHandlers(handlers) {
-        if (handlers.__bcg_patched) return;
-        handlers.__bcg_patched = true;
-
-        // patch username tapping to mention user instead
-        let tapUsernamePatch = Boolean(handlers.handleTapUsername)
-            && ReactNative.Platform.OS !== "android"
-            && instead("handleTapUsername", handlers, (args, orig) => {
-                if (!storage.tapUsernameMention) return orig.apply(this, args);
-
-                const ChatInput = ChatInputRef.refs[0].current;
-                const { messageId } = args[0].nativeEvent;
-
-                const message = MessageStore.getMessage(
-                    ChatInput.props?.channel?.id,
-                    messageId
-                )
-
-                if (!message) return;
-                ChatInputRef.insertText(`@${message.author.username}#${message.author.discriminator}`)
-            });
-
-        // patch tapping a message to require 2 taps and author and provide edit event if both conditions are met
-        let tapMessagePatch = Boolean(handlers.handleTapMessage) && after("handleTapMessage", handlers, (args) => {
-            const { nativeEvent }: { nativeEvent: DefaultNativeEvent } = args[0];
-            const ChannelID = nativeEvent.channelId;
-            const MessageID = nativeEvent.messageId;
-
-            this.currentTapIndex++;
-
-            let timeoutTap = setTimeout(() => {
-                this.currentTapIndex = 0;
-            }, storage.delay);
-
-            const channel = ChannelStore.getChannel(ChannelID);
-            const message = MessageStore.getMessage(ChannelID, MessageID);
-
-            Object.assign(nativeEvent, {
-                taps: this.currentTapIndex,
-                content: message?.content,
-                authorId: message?.author?.id,
-                isAuthor: message?.author?.id === UserStore.getCurrentUser()?.id
-            });
-
-            if (this.currentTapIndex !== 2)
-                return this.doubleTapState({
-                    state: "INCOMPLETE",
-                    nativeEvent
-                });
-
-            clearTimeout(timeoutTap);
-
-            if ((nativeEvent as NativeEvent)?.authorId === UserStore.getCurrentUser()?.id) {
-                if (storage.userEdit) {
-                    const MessageContent = (nativeEvent as NativeEvent).content;
-
-                    Messages.startEditMessage(
-                        ChannelID,
-                        MessageID,
-                        MessageContent
-                    );
-                } else {
-                    ReplyManager.createPendingReply({
-                        channel,
-                        message,
-                        shouldMention: true
-                    });
-                }
-
-                ChatInputRef.openSystemKeyboard()
-
-                return;
-            }
-
-            if (storage.reply) {
-                ReplyManager.createPendingReply({
-                    channel,
-                    message,
-                    shouldMention: true
-                })
-
-                ChatInputRef.openSystemKeyboard()
-            }
-
-            this.currentTapIndex = 0;
-            this.doubleTapState({
-                state: "COMPLETE",
-                nativeEvent
-            })
+function swipeLeftListener() {
+    // Function to handle swipe left
+    if (Drawer?.openDrawer && MembersList) {
+        Drawer.openDrawer("MemberList", {
+            render: MembersList,
         });
+    }
+}
 
-        this.unpatchHandlers = () => {
-            tapUsernamePatch && tapUsernamePatch();
-            tapMessagePatch && tapMessagePatch();
-        };
-    },
-
+export default registerPlugin({
+    name: 'Restore Swipe Left for Member List',
+    description: 'Restores the swipe left gesture to open the member list in Discord.',
+    version: '1.0.0',
+    authors: [{ name: 'NotTeganQuinn', id: 'NotTeganQuinn-ope' }],
+    
     onLoad() {
-        // initialize
-        storage.tapUsernameMention ??= ReactNative.Platform.select({
-            android: false,
-            ios: true,
-            default: true
-        });
-        storage.reply ??= true;
-        storage.userEdit ??= true;
-        storage.delay ??= 300;
-
-        const self = this;
-        const origGetParams = Object.getOwnPropertyDescriptor(MessagesHandlers.prototype, "params").get;
-
-        origGetParams && Object.defineProperty(MessagesHandlers.prototype, "params", {
-            configurable: true,
-            get() {
-                this && self.patchHandlers(this);
-                return origGetParams.call(this);
-            }
-        });
-
-
-        this.unpatchGetter = () => {
-            origGetParams && Object.defineProperty(MessagesHandlers.prototype, "params", {
-                configurable: true,
-                get: origGetParams
-            });
-        }
+        // Patch the swipe gesture to trigger swipeLeftListener when a left swipe is detected
+        patcher = before('handleGesture', Drawer, swipeLeftListener);
     },
 
     onUnload() {
-        this.unpatchGetter?.();
-        this.unpatchHandlers?.();
-    },
-
-    settings: Settings
+        // Remove the swipe gesture patch
+        if (patcher) patcher();
+    
 }
-
-export default BetterChatGestures;
+});
